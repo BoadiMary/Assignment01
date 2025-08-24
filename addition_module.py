c3132bb146fed91fb1f7e158cb134e9afca5b677
BoadiMary <boadimary205@gmail.com>
2025-08-24 21:03:56 +0000

    Add files via upload

---
+++ addition_module.py
@@ -0,0 +1,4 @@
+def sum():
+    num1=int(input("Enter first number "))
+    num2=int(input("Enter last number "))
+    return num1+num2
---
+++ imported_module.py
@@ -0,0 +1,2 @@
+import addition_module
+print(addition_module.sum())
